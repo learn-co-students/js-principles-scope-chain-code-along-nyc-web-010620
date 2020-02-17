b4f61b5b53dc014802fb94b25311c2d1f97ac3c5
myk245 <myk245@nyu.edu>
2020-02-17 12:12:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-/* Your code goes here */
+var customerName = "bob"
+const leastFavoriteCustomer = "will"
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+   bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+   bestCustomer = "maybe bob"; 
+}
+
+// unsuccessfully tries to reassign the least favorite customer
+// cannot change because we declared the least favorite customer as a constant
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = "not will"; 
+}
+
+// unsuccessfully tries to declare favoriteCustomer with let two times
+// had to comment out this function -- it breaks the rest of the tests
+// function attemptTwoFavoriteCustomers() {
+//    let favoriteCustomer = "sally"; 
+//    let favoriteCustomer = "allie"; 
+// }
